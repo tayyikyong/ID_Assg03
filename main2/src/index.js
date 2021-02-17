aa3ab1437ac098b73cc2471215eb7f1acb8682a2
tayyikyong <74086947+tayyikyong@users.noreply.github.com>
2021-02-17 20:32:55 +0800

    Added app.jsx, index.css, index.js

---
+++ main2/src/index.js
@@ -0,0 +1,5 @@
+import ReactDom from "react-dom";
+import App from "./App";
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+ReactDom.render(<App />, document.getElementById("root"));
