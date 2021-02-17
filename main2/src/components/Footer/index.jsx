f8b426a67f79184b9ebfe40a0b47cf0a5337ff0f
tayyikyong <74086947+tayyikyong@users.noreply.github.com>
2021-02-17 21:03:14 +0800

    Add index.jsx, style.css

---
+++ main2/src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const Footer = () => {
+  const date = new Date();
+  const fullYear = date.getFullYear();
+  return (
+    <footer className="footer">
+      <p>
+        All &copy; copy rights are reserved to Astro-shopping {fullYear}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
